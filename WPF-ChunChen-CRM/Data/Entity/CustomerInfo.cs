ba3b7ffa95b4b4512078b02a8334172095acda22
村长 <757891685@qq.com>
2018-09-11 09:29:02 +0800

    创建CustomerInfo，对应Customer客户信息表

---
+++ WPF-ChunChen-CRM/Data/Entity/CustomerInfo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entity
+{
+    /// <summary>
+    /// 客户信息表
+    /// </summary>
+    [Table("Customer")]
+    public partial class CustomerInfo
+    {
+        public Guid Id { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CustomerNo { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public int Gender { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Mobile { get; set; }
+
+        public string Address { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime? Birthday { get; set; }
+
+        /// <summary>
+        /// 服务人id
+        /// </summary>
+        public Guid? WaiterId { get; set; }
+
+        /// <summary>
+        /// 服务人名称
+        /// </summary>
+        public string WaiterName { get; set; }
+
+        /// <summary>
+        /// 消费总金额
+        /// </summary>
+        public double Spend { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public DateTime LastUpdatedOn { get; set; }
+
+        public bool Deleted { get; set; }
+
+
+    }
+}
