185691e72e0b99717987750ca849273c50ec12dd
村长 <757891685@qq.com>
2018-09-12 10:17:51 +0800

    创建RecordInfo，对应Record信息记录表

---
+++ WPF-ChunChen-CRM/Data/Entity/RecordInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entity
+{
+    /// <summary>
+    /// 信息记录表
+    /// </summary>
+    [Table("Record")]
+    public partial class RecordInfo
+    {
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// 客户id
+        /// </summary>
+        public Guid CustomerId { get; set; }
+
+        /// <summary>
+        /// 员工id
+        /// </summary>
+
+        public Guid EmployeeId { get; set; }
+
+        /// <summary>
+        /// 员工姓名
+        /// </summary>
+        [Required]
+        public string EmployeeName { get; set; }
+
+        /// <summary>
+        /// 记录
+        /// </summary>
+        [Required]
+        public string Message { get; set; }
+
+        public DateTime CreateDate { get; set; }
+    }
+}
