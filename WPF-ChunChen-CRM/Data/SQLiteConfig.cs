d0dbb16a711e69cb44e4367157b4e5543c7bb92f
村长 <757891685@qq.com>
2018-09-13 19:36:19 +0800

    创建SQLiteConfig连接SQLite数据库，连接字符串配置在App.config中

---
+++ WPF-ChunChen-CRM/Data/SQLiteConfig.cs
@@ -0,0 +1,32 @@
+﻿using Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    /// <summary>
+    /// 连接SQLite数据库
+    /// </summary>
+    public partial class SQLiteConfig : DbContext
+    {
+        public SQLiteConfig()
+            : base("SQLiteConfig")
+        {
+        }
+
+        public virtual DbSet<CustomerInfo> CustomerInfo { get; set; }
+        public virtual DbSet<EmployeeInfo> EmployeeInfo { get; set; }
+        public virtual DbSet<OrderInfo> OrderInfo { get; set; }
+        public virtual DbSet<AccountInfo> AccountInfo { get; set; }
+
+        public virtual DbSet<RecordInfo> RecordInfo { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
