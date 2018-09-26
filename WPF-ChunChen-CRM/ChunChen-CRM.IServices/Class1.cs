639bdca36aed3a12b12ee46023a58b5f12e37837
村长 <757891685@qq.com>
2018-09-26 16:35:30 +0800

    创建ChunChen-CRM.IServices，存放服务接口

---
+++ WPF-ChunChen-CRM/ChunChen-CRM.IServices/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChunChen_CRM.IServices
+{
+    public class Class1
+    {
+    }
+}
