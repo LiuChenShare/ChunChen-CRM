6fcd1a1865e96def997147ab1209fd7cf62198a6
村长 <757891685@qq.com>
2018-12-15 11:17:59 +0800

    创建IAccountService

---
+++ WPF-ChunChen-CRM/ChunChen-CRM.IServices/IAccountService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChunChen_CRM.IServices
+{
+    /// <summary>
+    /// 账户相关业务接口
+    /// </summary>
+    public interface IAccountService
+    {
+        /// <summary>
+        /// 系统登录
+        /// </summary>
+        /// <param name="account"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        bool Login(string account, string password);
+
+        /// <summary>
+        /// 系统登出
+        /// </summary>
+        void Logout();
+    }
+}
