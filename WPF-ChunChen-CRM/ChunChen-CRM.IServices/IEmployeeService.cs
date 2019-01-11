2606191449c73db0ef2d55d478eb494431447f24
村长 <757891685@qq.com>
2019-01-11 16:57:32 +0800

    提交IEmployeeService员工相关业务接口

---
+++ WPF-ChunChen-CRM/ChunChen-CRM.IServices/IEmployeeService.cs
@@ -0,0 +1,21 @@
+﻿using ChunChen_CRM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChunChen_CRM.IServices
+{
+    /// <summary>
+    /// 员工相关业务接口
+    /// </summary>
+    public interface IEmployeeService
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        UserViewModel GetPersonalData();
+    }
+}
