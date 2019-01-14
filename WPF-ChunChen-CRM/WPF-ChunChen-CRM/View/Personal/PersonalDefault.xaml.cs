57ecaf9b5362d80f963f9a422d785f82824ef921
村长 <757891685@qq.com>
2019-01-14 17:32:40 +0800

    完成个人信息页面的数据绑定

--- WPF-ChunChen-CRM/WPF-ChunChen-CRM/View/Personal/PersonalDefault.xaml.cs
+++ WPF-ChunChen-CRM/WPF-ChunChen-CRM/View/Personal/PersonalDefault.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ChunChen_CRM.IServices;
+using ChunChen_CRM.Model;
+using ChunChen_CRM.Services;
+using System;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -13,8 +16,19 @@ namespace WPF_ChunChen_CRM.View.Personal
         public PersonalDefault()
         {
             InitializeComponent();
+            UpdateUserPersonalData();
         }
 
+
+        #region 服务
+        private IEmployeeService employeeService = new EmployeeService();
+        #endregion
+
+        #region 参数
+
+        #endregion
+
+        #region 按钮
         private void EditButton_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new Uri("View/Personal/PersonalEdit.xaml", UriKind.Relative));
@@ -29,5 +43,32 @@ namespace WPF_ChunChen_CRM.View.Personal
         {
             //this.ma.PersonalFarme.Navigate(new Uri("View/Personal/PersonalDefault.xaml", UriKind.Relative));
         }
+        #endregion
+
+        #region 方法
+        /// <summary>
+        /// 更新用户个人信息
+        /// </summary>
+        private void UpdateUserPersonalData()
+        {
+            UserViewModel userViewModel = employeeService.GetPersonalData();
+            EmployeeNoValue.Content = userViewModel.EmployeeNo;
+            NameValue.Content = userViewModel.Name;
+            MobileValue.Content = userViewModel.Mobile;
+            GenderStringValue.Content = userViewModel.Mobile;
+            BirthdayValue.Content = userViewModel.Birthday?.ToString("yyyy-MM-dd");
+            if (userViewModel.Quit)
+            {
+                JoinDateName.Text = "离职日期：";
+                JoinDateValue.Content = userViewModel.QuitDate?.ToString("yyyy-MM-dd");
+            }
+            else
+            {
+                JoinDateName.Text = "入职日期：";
+                JoinDateValue.Content = userViewModel.JoinDate.ToString("yyyy-MM-dd");
+            }
+        }
+        #endregion
+
     }
 }
