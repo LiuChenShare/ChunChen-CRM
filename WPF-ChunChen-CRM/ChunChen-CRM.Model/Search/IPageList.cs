5b3dcc2149d2b39f545288b76a1cc525bd4f6f62
村长 <757891685@qq.com>
2019-01-29 11:15:25 +0800

    添加IPageList及PageList用于分页数据

---
+++ WPF-ChunChen-CRM/ChunChen-CRM.Model/Search/IPageList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChunChen_CRM.Model.Search
+{
+    public interface IPageList<T>
+    {
+    }
+}
---
+++ WPF-ChunChen-CRM/ChunChen-CRM.Model/Search/PageList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChunChen_CRM.Model.Search
+{
+    public class PageList<T> : IPageList<T>
+    {
+        /// <summary>
+        /// 页码
+        /// </summary>
+        public int PageIndex { get; set; }
+        
+        /// <summary>
+        /// 每页数量
+        /// </summary>
+        public int PageSize { get; set; }
+
+        /// <summary>
+        /// 总页码
+        /// </summary>
+        public int TotalPage { get; set; }
+
+        /// <summary>
+        /// 总数量
+        /// </summary>
+        public int TotalCount { get; set; }
+
+        /// <summary>
+        /// 数据
+        /// </summary>
+        public IList<T> Data;
+    }
+}
